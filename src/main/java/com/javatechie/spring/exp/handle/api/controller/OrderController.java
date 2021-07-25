b391fbbcfdedd09cd2589523356713ce8d528211
venkata subbareddy k <venkata subbareddy k@DESKTOP-QPNSBTO>
2021-07-25 17:04:04 +0530

    Intial commit

---
+++ src/main/java/com/javatechie/spring/exp/handle/api/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.javatechie.spring.exp.handle.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javatechie.spring.exp.handle.api.exception.OrderServiceException;
+import com.javatechie.spring.exp.handle.api.service.OrderService;
+
+@RestController
+public class OrderController {
+	@Autowired
+	private OrderService service;
+//http://localhost:8080/getPrice/Bike
+//http://localhost:8080/getPrice/Bikees(Error message display)
+	@GetMapping("/getPrice/{productName}")
+	public String getOrderPrice(@PathVariable String productName) throws OrderServiceException {
+		double amount = service.getPrice(productName);
+		return "amount for product " + productName + " is " + amount;
+	}
+}
